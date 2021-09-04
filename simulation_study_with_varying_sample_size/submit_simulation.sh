45d3b9845f718009c9ba6ac300e5cb60a530f1f1
Aki Nishimura <aki.nishimura@jhu.edu>
2021-09-04 15:30:39 -0400

    Dump output and errors into same file
    separate log files seem overkill.

--- simulation_study_with_varying_sample_size/submit_simulation.sh
+++ simulation_study_with_varying_sample_size/submit_simulation.sh
@@ -5,16 +5,15 @@ n_core=2
 for sample_size in 1000 4000 16000; do
   for method_type in 'old' 'new'; do
     job_name="${method_type}_method_simulation_with_${sample_size}_observations"
-    output_log_file_name="${HOME}/${job_name}_qsub_output_log.txt"
-    error_log_file_name="${HOME}/${job_name}_qsub_error_log.txt"
+    log_file_name="${HOME}/${job_name}_qsub_log.txt"
     echo "Starting ${job_name}..."
     qsub \
       -q "${node_name}" `# If you need a specific node` \
       -l mem_free="${mem_gb_free}G",h_vmem="${mem_gb_free}G" `# Specify memory requirement` \
       -pe local $n_core `# Parallel environment for multi-threading` \
       -N $job_name `# Give a human-readable name to the submitted job so that you can find it later` \
-      -o $output_log_file_name \
-      -e $error_log_file_name \
+      -o $log_file_name `# Direct output messages` \
+      -e $log_file_name `# Direct errors` \
       -m e -M your.email@jh.edu `# Send an email when the job completes or aborts` \
       -v method_type=${method_type},sample_size=${sample_size},job_name=${job_name} `# Assign variables to be passed to the bash script` \
       run_single_simulation.sh
